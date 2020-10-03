1cd7ee2a2d16c409983ef97d4d594f858cc99929
İbrahim Özdemir <ibrahim.ozdemir@outlook.com.tr>
2020-10-03 18:36:43 +0300

    BulkInsert controller and little fixes

---
+++ TaxesAPI/Controllers/TaxesBulkInsertController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaxesAPI.Services;
+
+namespace TaxesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaxesBulkInsertController : ControllerBase
+    {
+        private readonly ITaxesService _businessService;
+
+
+
+        public TaxesBulkInsertController( ITaxesService businessService)
+        {
+            _businessService = businessService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> ReadCVS(string path) //This Implementation has missing part from reading file
+        {
+            return await _businessService.ReadCSVAsync(path);
+        }
+    }
+    
+}
